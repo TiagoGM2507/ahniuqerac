523ee69a13456d663dad05f3104e517d5675c175
zStormz <58051508+zStormz@users.noreply.github.com>
2022-08-28 18:31:01 -0300

    função quick sort
    Função para ordenação dos numeros

--- main.c
+++ main.c
@@ -17,6 +17,33 @@ int bubble_sort(int numeros[]){
       }
   return numeros[1000];
 }
+
+//função quick sort
+int quick_sort(int numeros[],int primeiro,int ultimo){
+   int i, j, pivot, temp;
+   if(primeiro<ultimo){
+      pivot=primeiro;
+      i=primeiro;
+      j=ultimo;
+      while(i<j){
+         while(numeros[i]<=numeros[pivot]&&i<ultimo)
+         i++;
+         while(numeros[j]>numeros[pivot])
+         j--;
+         if(i<j){
+            temp=numeros[i];
+            numeros[i]=numeros[j];
+            numeros[j]=temp;
+         }
+      }
+      temp=numeros[pivot];
+      numeros[pivot]=numeros[j];
+      numeros[j]=temp;
+      quick_sort(numeros,primeiro,j-1);
+      quick_sort(numeros,j+1,primeiro);
+   }
+  return numeros[1000];
+}
   
 int main()
 {
