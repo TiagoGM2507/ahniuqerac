f8bb6763a8e80828831a64304ce4911d777860d9
João Tápparo <joaovtapparo@gmail.com>
2022-08-28 18:25:39 -0300

    função bubble sort

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h> // srand, rand
+#include <time.h> // time
+
+//função bubble sort
+int bubble_sort(int numeros[]){
+
+    int i, r, aux;
+      for(i = 0; i < 1000; i++){
+          for(r = i; r < 1000; r++){
+                if (numeros[i] > numeros[r]){
+                   aux = numeros[r];
+                   numeros[r] = numeros[i];
+                   numeros[i] = aux;
+                  }
+          }  
+      }
+  return numeros[1000];
+}
   
 int main()
 {
